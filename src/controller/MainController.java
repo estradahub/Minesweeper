f4f03068fb6fcef4992e0f5bde80039a715a5b14
Bryan Arredondo <le__bryan@hotmail.com>
2021-04-27 22:25:25 -0500

    Intermediate button updated

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -22,36 +22,46 @@ import model.matrizSecondForm;
 public class MainController {
 	
 	public Button beginner, intermediate, hard, perso;
-	//private GameController gc = new GameController(); 
 	private matrizSecondForm mat = new matrizSecondForm();
 
 	public void beginnerButtonAction(ActionEvent event) throws IOException {
 	
-		Random r = new Random();								
-		
+		Random r = new Random();
+		int bombsQuantity = 10;
+		//Button tile = new Button();
 		GridPane grid = new GridPane();
-		
-		for (int i=0;i<8;i++) {
-			for (int j=0;j<8;j++) {
-				int bomb = r.nextInt(2);
-				String oneForBomb = String.format(" %s ", bomb);
-				Button tile = new Button(oneForBomb);
-				
-				tile.setOnAction(new EventHandler<ActionEvent>(){					
-					@Override
-					public void handle(ActionEvent arg0) {						
-						System.out.println("It is working now");
-					}					
-				});
-				
-				grid.add(tile, i, j);
-			}			
+		while (bombsQuantity > 0) {
+			
+			for (int i=0;i<8;i++) {
+				for (int j=0;j<8;j++) {
+					Button tile = new Button();
+					int bomb = r.nextInt(4);
+					if(bombsQuantity > 0 && bomb == 1 && tile.getText() != "1") {
+						bombsQuantity--;						
+						String oneForBomb = String.format("%s", bomb);
+						tile.setText(oneForBomb);
+					}else {
+						tile.setText("  ");
+					}
+					//String oneForBomb = String.format(" %s ", bomb);
+									
+					tile.setOnAction(new EventHandler<ActionEvent>(){					
+						@Override
+						public void handle(ActionEvent arg0) {						
+							System.out.println("It is working now");
+						}					
+					});
+					
+					grid.add(tile, i, j);
+				}	
+			}				
 		}
 
-		Scene scene = new Scene(grid,800,600);
+		Scene scene = new Scene(grid,580,450);
 		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
 		window.setScene(scene);
 		window.show();
+
 
 	}
 	public void tileOnAction(ActionEvent event)throws IOException {
@@ -60,32 +70,34 @@ public class MainController {
 	}
 	public void intermediateButtonAction(ActionEvent event) throws IOException {
 		Random r = new Random();
-		int bombsQuantity = 10;
+		int bombsQuantity = 40;
 		//Button tile = new Button();
 		GridPane grid = new GridPane();
-		
-		for (int i=0;i<16;i++) {
-			for (int j=0;j<16;j++) {
-				Button tile = new Button();
-				int bomb = r.nextInt(16);
-				if(bombsQuantity > 0 && bomb == 1) {
-					bombsQuantity--;
-					String oneForBomb = String.format("%s", bomb);
-					tile.setText(oneForBomb);
-				}else {
-					tile.setText("  ");
-				}
-				//String oneForBomb = String.format(" %s ", bomb);
-								
-				tile.setOnAction(new EventHandler<ActionEvent>(){					
-					@Override
-					public void handle(ActionEvent arg0) {						
-						System.out.println("It is working now");
-					}					
-				});
-				
-				grid.add(tile, i, j);
-			}			
+		while (bombsQuantity > 0) {
+			
+			for (int i=0;i<16;i++) {
+				for (int j=0;j<16;j++) {
+					Button tile = new Button();
+					int bomb = r.nextInt(4);
+					if(bombsQuantity > 0 && bomb == 1 && tile.getText() != "1") {
+						bombsQuantity--;						
+						String oneForBomb = String.format("%s", bomb);
+						tile.setText(oneForBomb);
+					}else {
+						tile.setText("  ");
+					}
+					//String oneForBomb = String.format(" %s ", bomb);
+									
+					tile.setOnAction(new EventHandler<ActionEvent>(){					
+						@Override
+						public void handle(ActionEvent arg0) {						
+							System.out.println("It is working now");
+						}					
+					});
+					
+					grid.add(tile, i, j);
+				}	
+			}				
 		}
 
 		Scene scene = new Scene(grid,580,450);
@@ -95,11 +107,41 @@ public class MainController {
 
 	}
 	public void hardButtonAction(ActionEvent event) throws IOException {
-		Parent gameParent = FXMLLoader.load(getClass().getResource("../view/Game.fxml"));
-		Scene gameScene = new Scene(gameParent);						
-		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();		
-		window.setScene(gameScene);
+		Random r = new Random();
+		int bombsQuantity = 99;		
+		GridPane grid = new GridPane();
+		while (bombsQuantity > 0) {
+			
+			for (int i=0;i<16;i++) {
+				for (int j=0;j<30;j++) {
+					Button tile = new Button();
+					int bomb = r.nextInt(4);
+					if(bombsQuantity > 0 && bomb == 1 && tile.getText() != "1") {
+						bombsQuantity--;						
+						String oneForBomb = String.format("%s", bomb);
+						tile.setText(oneForBomb);
+					}else {
+						tile.setText("  ");
+					}
+					//String oneForBomb = String.format(" %s ", bomb);
+									
+					tile.setOnAction(new EventHandler<ActionEvent>(){					
+						@Override
+						public void handle(ActionEvent arg0) {						
+							System.out.println("It is working now");
+						}					
+					});
+					
+					grid.add(tile, i, j);
+				}	
+			}				
+		}
+
+		Scene scene = new Scene(grid,580,450);
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		window.setScene(scene);
 		window.show();
+
 	}
 	public void persoButtonAction(ActionEvent event) throws IOException {
 		Parent gameParent = FXMLLoader.load(getClass().getResource("../view/Game.fxml"));
